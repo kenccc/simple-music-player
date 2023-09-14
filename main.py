c6293b1f432e1ffcf45529cf652bcc2ea56010ce
Ken Vu <kenvu5530@gmail.com>
2023-09-14 16:56:44 +0200

    added pause/resume button, working queue

--- main.py
+++ main.py
@@ -6,22 +6,63 @@ from pathlib import Path
 from tkinter.filedialog import askopenfile
 
 root = Tk()
+pygame.init()
 pygame.mixer.init()
+MUSIC_END = pygame.USEREVENT+1
+pygame.mixer.music.set_endevent(MUSIC_END)
+
+queue = []
+
+
+def check_event():
+    for event in pygame.event.get():
+        if event.type == MUSIC_END:
+            print('music end event')
+            PlayMusic()
+
+
+    root.after(100, check_event)
 def browse():
     file = filedialog.askopenfile(mode="r")
     if file:
         global filepath
         filepath = os.path.abspath(file.name)
-        print(filepath)
+        queue.append(filepath)
+        print(queue)
+
 def PlayMusic():
-    pygame.mixer.music.load(filepath)
-    fileName = Path(filepath)
-    songlabel.config(text=f"Playing:\n{fileName.name}")
+    pygame.mixer.music.load(queue.pop(0))
     pygame.mixer.music.play(loops=0, start=0.0, fade_ms=0)
+def updateUI():
+    fileNames = [Path(path).name for path in queue]
+    songlabel.config(text="\n".join(fileNames))
+    root.after(100, updateUI)
+
+
+clicked = 0
+
+
+def PauseMusic():
+    global clicked
+    clicked += 1
+    if clicked == 1:
+        pauseBtn.config(text='resume')
+        pygame.mixer.music.pause()
+    if clicked == 2:
+        clicked = 0
+        pauseBtn.config(text='pause')
+        pygame.mixer.music.unpause()
+    print(clicked)
+queueLabel=Label(
+    master=root,
+    text="Queue:"
+)
+queueLabel.pack(pady=0, padx =0)
 songlabel = Label(
     master=root,
 )
 songlabel.pack(pady=0, padx =0)
+
 browseBtn = Button(
     master=root,
     command=browse,
@@ -35,6 +76,16 @@ playBtn = Button(
     text="play",
 )
 
+pauseBtn = Button(
+    master=root,
+    command=PauseMusic,
+    text='pause'
+)
+
+pauseBtn.pack(padx=0,pady=0)
 playBtn.pack(padx=0,pady=0)
+check_event()
+updateUI()
 root.mainloop()
+pygame.quit()
 
